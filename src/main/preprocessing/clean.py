1e26821953ad72940436c6ee4e209f4dcbdf8fa5
Jazio Liu <winnieliup@gmail.com>
2021-09-07 14:59:21 +0800

    YO

--- src/main/preprocessing/clean.py
+++ src/main/preprocessing/clean.py
@@ -3,32 +3,27 @@ import os
 from datetime import datetime, timedelta
 
 import pandas as pd
-
 from utility import logger
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 ROOT_RAW = f'{CURRENT_DIR}/data/00_raw/'
 ROOT_CLEAN = f'{CURRENT_DIR}/data/01_clean/'
-DROPPABLE_INVERTER_COL_LIST = [
-    'in_voltage_0', 'in_current_0', 'out_current', 'out_voltage',
-    'out_frequency', 'operation_hour', 'phase_r_t', 'phase_r_i', 'phase_r_v',
-    'phase_r_f', 'phase_r_p', 'phase_s_t', 'phase_s_i', 'phase_s_v',
-    'phase_s_f', 'phase_s_p', 'phase_t_t', 'phase_t_i', 'phase_t_v',
-    'phase_t_f', 'phase_t_p', 'power_factor_pf', 'power_factor_va',
-    'power_factor_var', 'power_factor_kvar', 'power_factor_kvarh', 'create_time'
-]
+DROPPABLE_INVERTER_COL_LIST = ['in_voltage_0', 'in_current_0', 'out_current',
+                               'out_voltage', 'out_frequency', 'operation_hour', 'phase_r_t', 'phase_r_i',
+                               'phase_r_v', 'phase_r_f', 'phase_r_p', 'phase_s_t', 'phase_s_i',
+                               'phase_s_v', 'phase_s_f', 'phase_s_p', 'phase_t_t', 'phase_t_i',
+                               'phase_t_v', 'phase_t_f', 'phase_t_p', 'power_factor_pf', 'power_factor_va',
+                               'power_factor_var', 'power_factor_kvar', 'power_factor_kvarh', 'create_time'
+                               ]
 PRESERVABLE_IRRADIANCE_COL_LIST = ['site_id', 'data_time', 'value_1']
 PRESERVABLE_THERMAL_COL_LIST = ['site_id', 'data_time', 'value_1']
 INVERTER_CODE_COL_LIST = [
     'errcodee', 'errcodef', 'errcodeg', 'errcodeh', 'wcode1', 'wcode2',
-    'wcode3', 'fcode1', 'fcode2', 'fcode3', 'fcode4', 'fcode5'
-]
-INVERTER_CURRENT_AND_POWER_LIST = [
-    'in_current_1', 'in_current_2', 'in_current_3', 'in_current_4',
-    'in_current_5', 'in_current_6', 'in_current_7', 'in_current_8', 'out_power',
-    'power_lifetime'
-]
+    'wcode3', 'fcode1', 'fcode2', 'fcode3', 'fcode4', 'fcode5']
+INVERTER_CURRENT_AND_POWER_LIST = ['in_current_1', 'in_current_2',
+                                   'in_current_3', 'in_current_4', 'in_current_5', 'in_current_6',
+                                   'in_current_7', 'in_current_8', 'out_power', 'power_lifetime']
 
 START_IDX_OF_TIMEZONE = -6
 
@@ -47,14 +42,17 @@ def rescale_datetimeindex(objs, startdt, enddt):
     return objs.reindex(rescaled_index)
 
 
-def is_first_or_last_datadt_in_margin(dt, earliest_time, latest_time, config):
+def is_first_or_last_datadt_in_margin(dt,
+                                      earliest_time,
+                                      latest_time,
+                                      config):
     margin_mins = \
         config['preprocessing']['missing_margin_data_replace_max_duration_mins']
 
     margin_mins = int(margin_mins)
 
-    delta_in_start_and_earliest = (dt.index[0] - earliest_time).seconds / 60
-    delta_in_end_and_latest = (latest_time - dt.index[-1]).seconds / 60
+    delta_in_start_and_earliest = (dt.index[0] - earliest_time).seconds/60
+    delta_in_end_and_latest = (latest_time - dt.index[-1]).seconds/60
 
     if delta_in_start_and_earliest <= margin_mins and \
             delta_in_end_and_latest <= margin_mins:
@@ -128,7 +126,8 @@ def do_clean_inverter(src_df, site_id, config):
                                              config) and \
                 is_nan_num_under_threshold(tmp_df.index, config):
 
-            tmp_df = rescale_datetimeindex(tmp_df, tmp_df.index[0],
+            tmp_df = rescale_datetimeindex(tmp_df,
+                                           tmp_df.index[0],
                                            tmp_df.index[-1])
             tmp_df.loc[:, INVERTER_CODE_COL_LIST] = \
                 tmp_df.loc[:, INVERTER_CODE_COL_LIST].fillna(method='ffill')
@@ -138,10 +137,11 @@ def do_clean_inverter(src_df, site_id, config):
                     tmp_df.loc[:, INVERTER_CURRENT_AND_POWER_LIST])
 
             col = tmp_df.columns.symmetric_difference(
-                INVERTER_CODE_COL_LIST + INVERTER_CURRENT_AND_POWER_LIST +
-                ['device_id', 'site_id'])
+                INVERTER_CODE_COL_LIST + INVERTER_CURRENT_AND_POWER_LIST
+                + ['device_id', 'site_id'])
 
-            tmp_df.loc[:, col] = tmp_df.loc[:, col].interpolate(method='linear')
+            tmp_df.loc[:, col] = tmp_df.loc[:,
+                                            col].interpolate(method='linear')
 
             tmp_df = rescale_datetimeindex(tmp_df, earliest_dt, latest_dt)
 
@@ -189,7 +189,8 @@ def clean_thermal_irradiance(table_name, src_df, site_id, config):
                                          config) and \
             is_nan_num_under_threshold(rtn_df.index, config):
 
-        rtn_df = rescale_datetimeindex(rtn_df, rtn_df.index[0],
+        rtn_df = rescale_datetimeindex(rtn_df,
+                                       rtn_df.index[0],
                                        rtn_df.index[-1])
 
         rtn_df[table_name].interpolate(method='linear', inplace=True)
@@ -251,8 +252,8 @@ def main():
 
                     if len(src_df) == 0:
                         logger.info(
-                            f'Empty data, skip it, site_id[{site_id}], ' +
-                            f'table_name[{table_name}], file[{file}]')
+                            f'Empty data, skip it, site_id[{site_id}], '
+                            + f'table_name[{table_name}], file[{file}]')
                         continue
 
                     df = do_clean(src_df, site_id, config)
@@ -260,13 +261,14 @@ def main():
                     df.to_csv(os.path.join(dst_path, file),
                               index=False,
                               mode='w')
-                    logger.info(f'clean completed, site_id[{site_id}], ' +
-                                f'table_name[{table_name}], ' +
-                                f'file[{file},({idx+1}/{len(file_list)})]')
+                    logger.info(
+                        f'clean completed, site_id[{site_id}], '
+                        + f'table_name[{table_name}], '
+                        + f'file[{file},({idx+1}/{len(file_list)})]')
 
     except Exception:
-        logger.exception(f'Failed to clean data, site_id[{site_id}], ' +
-                         f'table_name[{table_name}], file[{file}]')
+        logger.exception(f'Failed to clean data, site_id[{site_id}], '
+                         + f'table_name[{table_name}], file[{file}]')
 
 
 if __name__ == '__main__':
